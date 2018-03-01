764c5f378d8f7e7d48d0b204649a02d2273f0d29
sushantahuja <sushant.ahuja@tcu.edu>
2018-03-01 15:58:30 -0600

    changed one line

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function App() {
     <div>
       <Welcome name="Greetings to Sara" />
       <Welcome name="Cahal Greetings" />
-      <Welcome name="Welcome Edite to github" />
+      <Welcome name="Edite is loving github" />
     </div>
   );
 }
